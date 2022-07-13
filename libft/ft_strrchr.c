cad68cc0d15e2a544136b45887bd717db905c49f
Seonggyu Kim <seonggyu@kakao.com>
2022-07-13 18:22:19 +0900

    Clear...

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: seonggyk <seonggyk@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 23:09:35 by seonggyk          #+#    #+#             */
-/*   Updated: 2022/07/08 13:54:26 by seonggyk         ###   ########.fr       */
+/*   Updated: 2022/07/13 18:17:29 by seonggyk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_strrchr(const char *s, int c)
 	if (c == 0)
 		return ((char *)ps);
 	while (s != ps--)
-		if (*ps == c)
+		if (*ps == (char)c)
 			return ((char *)ps);
 	return (0);
 }
