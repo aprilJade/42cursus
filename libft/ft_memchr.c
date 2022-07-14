9aa8e728bd1b75e247c0db43747b23cceda7a13d
Seonggyu Kim <seonggyu@kakao.com>
2022-07-14 11:53:06 +0900

    Clear....

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: seonggyk <seonggyk@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 23:20:39 by seonggyk          #+#    #+#             */
-/*   Updated: 2022/07/14 11:43:36 by seonggyk         ###   ########.fr       */
+/*   Updated: 2022/07/14 11:49:37 by seonggyk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char	*hs;
+
 	hs = (unsigned char *)s;
 	while (n--)
 		if (*hs++ == (unsigned char)c)
