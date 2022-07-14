a13234f58da5a9999fa71f4c294f3f86abb42222
Seonggyu Kim <seonggyu@kakao.com>
2022-07-14 15:16:06 +0900

    Fix

--- libft/ft_putstr_fd.c
+++ libft/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: seonggyk <seonggyk@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 15:36:43 by seonggyk          #+#    #+#             */
-/*   Updated: 2022/07/14 14:07:49 by seonggyk         ###   ########.fr       */
+/*   Updated: 2022/07/14 15:15:42 by seonggyk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,6 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
-	int	len;
-	int	sum;
-	int	result;
-
 	if (s == NULL || fd < 0)
 		return ;
 	while (*s)
