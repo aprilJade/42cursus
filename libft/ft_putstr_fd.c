4af90db91a0a9166465289ea33f082d5c7334aca
Seonggyu Kim <seonggyu@kakao.com>
2022-07-14 14:25:15 +0900

    Fix...

--- libft/ft_putstr_fd.c
+++ libft/ft_putstr_fd.c
@@ -6,21 +6,11 @@
 /*   By: seonggyk <seonggyk@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 15:36:43 by seonggyk          #+#    #+#             */
-/*   Updated: 2022/07/08 20:08:54 by seonggyk         ###   ########.fr       */
+/*   Updated: 2022/07/14 14:07:49 by seonggyk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
-static int	ft_strlen(char *s)
-{
-	char	*str;
-
-	str = s;
-	while (*s)
-		s++;
-	return (s - str);
-}
 
 void	ft_putstr_fd(char *s, int fd)
 {
@@ -30,14 +20,6 @@ void	ft_putstr_fd(char *s, int fd)
 
 	if (s == NULL || fd < 0)
 		return ;
-	len = ft_strlen(s);
-	result = 0;
-	sum = 0;
-	while (sum != len)
-	{
-		s += sum;
-		len -= sum;
-		result = write(fd, s, len);
-		sum += result;
-	}
+	while (*s)
+		write(fd, s++, 1);
 }
